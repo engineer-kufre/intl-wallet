f56c04beadc2ae5349b0cb5cac13e71df72b6607
Kufreabasi <engineer_kufre@yahoo.com>
2021-01-09 18:20:18 +0100

    created signup endpoint

---
+++ IntlWallet/IntlWallet.API/Controllers/AuthController.cs
@@ -0,0 +1,59 @@
+﻿using IntlWallet.DTOs;
+using IntlWallet.Models;
+using IntlWallet.Utils;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IntlWallet.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly ILogger<AuthController> _logger;
+
+        public AuthController(UserManager<ApplicationUser> userManager, ILogger<AuthController> logger)
+        {
+            _userManager = userManager;
+            _logger = logger;
+        }
+
+        [HttpPost("signup")]
+        public async Task<IActionResult> Register(UserToSignUpDTO model)
+        {
+            // create user
+            ApplicationUser newUser = new ApplicationUser
+            {
+                Id = Guid.NewGuid().ToString(),
+                UserName = model.Email,
+                Email = model.Email,
+                LastName = model.LastName,
+                FirstName = model.FirstName,
+                PhoneNumber = model.PhoneNumber
+            };
+
+            var result = await _userManager.CreateAsync(newUser, model.Password);
+            if (result.Succeeded)
+            {
+                await _userManager.AddToRoleAsync(newUser, model.Role);
+            }
+            else
+            {
+                foreach (var err in result.Errors)
+                {
+                    ModelState.AddModelError("", err.Description);
+                }
+                return BadRequest(ResponseMessage.Message("Bad request", errors: new { message = ModelState }));
+            }
+
+            return Ok(ResponseMessage.Message("Success! User created", data: new {newUser.Id }));
+        }
+    }
+}
---
+++ IntlWallet/IntlWallet.DTOs/UserToSignUpDTO.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace IntlWallet.DTOs
+{
+    public class UserToSignUpDTO
+    {
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Phone number")]
+        [MaxLength(14, ErrorMessage = "Phone number must not be 14 characters")]
+        [RegularExpression(@"^\+\d{3}\d{9,10}$", ErrorMessage = "Must have country-code and must be 13, 14 chars long e.g. +2348050000000")]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public string Role { get; set; }
+    }
+}
