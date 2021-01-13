0d60469e8d6dc1656c09fd6a37d49770b202116c
Kufreabasi <engineer_kufre@yahoo.com>
2021-01-14 00:24:04 +0100

    added some utils unit tests

---
+++ IntlWallet/IntlWallet.Tests/TestMethods/UtilsTests.cs
@@ -0,0 +1,48 @@
+﻿using IntlWallet.Data.Services;
+using IntlWallet.Models;
+using IntlWallet.Utils;
+using Microsoft.EntityFrameworkCore.SqlServer.Storage.Internal;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IntlWallet.Tests.TestMethods
+{
+    public class UtilsTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+            
+        }
+
+        [Test]
+        public void InvalidCurrencyThrowsArgumentException()
+        {
+            //Arrange
+            var transactionCurrency = "EUA";
+            var mainCurrency = "NGN";
+            var amount = 1;
+
+            //Assert
+            Assert.Throws<AggregateException>(() => CurrencyConverter.ConvertCurrency(transactionCurrency, mainCurrency, amount).Wait());
+        }
+
+        [Test]
+        public void InvalidCurrencyInputReturnsFalse()
+        {
+            //Arrange
+            var currency = "USA";
+            var expected = false;
+
+            //Act
+            var actual = CurrencyConverter.ValidateCurrencyInput(currency);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
